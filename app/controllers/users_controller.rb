c4238ad3ea74d9f0408455068bd10f9d7f506246
NguyenThuong <thuongnn1304@gmail.com>
2019-06-20 15:48:56 +0700

    fixed chapter_7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,27 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+    return @user.nil?
+    render html: "not found"
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = t("auth.success", email: @user.email)
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
 end
